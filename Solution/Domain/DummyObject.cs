6ed3f6e13f831cd7919325d4a6d97d8b73aa68a2
notauserx <notauserx@users.noreply.github.com>
2023-03-01 10:48:41 +0600

    add dummy object to demo value object

---
+++ Solution/Domain/DummyObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Solution.Domain;
+
+/// <summary>
+/// A value object for demo purposes
+/// </summary>
+public class DummyObject : ValueObject
+{
+    public string Value { get; private set; }
+
+    public DummyObject(string value)
+    {
+        Value = value;
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
