035d4153ac5a55ada5bdc81bd60763bf340890b3
notauserx <notauserx@users.noreply.github.com>
2023-03-01 12:23:31 +0600

    add user stories 1, 2 and 3

---
+++ Solution.Tests/ShoppingCartTests.cs
@@ -0,0 +1,148 @@
+﻿namespace Solution.Tests;
+
+public class ShoppingCartTests
+{
+    [Fact]
+    public void shopping_cart_is_initially_empty()
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        Assert.Equal(0, cart.GetNumberOfProducts());
+    }
+
+    [Fact]
+    public void when_a_product_is_added_to_the_cart_the_number_of_products_is_increased()
+    {
+        ShoppingCart cart = ShoppingCart.CreateEmptyCart();
+
+        var currentProductCount = 0;
+        Assert.Equal(currentProductCount, cart.GetNumberOfProducts());
+
+        cart.StoreProduct(new Product(new ProductName("Product A"),
+                                      ProductPrice.FromDecimal(1),
+                                      ProductQuantity.FromInt(1),
+                                      null));
+
+        currentProductCount++;
+        Assert.Equal(currentProductCount, cart.GetNumberOfProducts());
+    }
+
+    [Fact]
+    public void sum_of_items_returns_the_sum_of_product_prices()
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        cart.StoreProduct(new Product(new ProductName("T-Shirt"),
+                                      ProductPrice.FromDecimal(10),
+                                      ProductQuantity.FromInt(1),
+                                      null));
+
+        cart.StoreProduct(new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(1),
+                                      null));
+
+        Assert.Equal(30, cart.GetSumOfItems());
+
+    }
+
+    [Fact]
+    public void products_can_have_discounts()
+    {
+
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(1),
+                                      new ThreeForThePriceOfTwo()
+                                );
+
+        Assert.True(discountedProduct.HasADiscount);
+    }
+
+    [Fact]
+    public void products_can_have_no_discounts()
+    {
+
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(1),
+                                      null
+                                );
+
+        Assert.False(discountedProduct.HasADiscount);
+    }
+
+}
+
+public class ThreeForThePriceOfTwoDiscountTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void when_less_than_three_items_discount_does_not_apply(int quantity)
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(pricePerItem),
+                                      ProductQuantity.FromInt(quantity),
+                                      new ThreeForThePriceOfTwo()
+                                );
+
+        var expected = pricePerItem * quantity;
+
+        Assert.Equal(expected, discountedProduct.GetPriceWithDiscount());
+    }
+
+    [Theory]
+    [InlineData(6)]
+    [InlineData(30)]
+    [InlineData(18)]
+    [InlineData(3)]
+    public void when_quantity_is_a_multiple_of_three_discount_applies_to_each_multiple(int quantity)
+    {
+        var pricePerItem = 20;
+        var multipleOfThree = quantity / 3;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(pricePerItem),
+                                      ProductQuantity.FromInt(quantity),
+                                      new ThreeForThePriceOfTwo()
+                                );
+
+        var expected = pricePerItem * 2 * multipleOfThree;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+
+    [Fact]
+    public void when_quantity_is_four_three_items_get_discounted_and_one_item_gets_no_discount()
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                    ProductPrice.FromDecimal(pricePerItem),
+                                     ProductQuantity.FromInt(4),
+                                     new ThreeForThePriceOfTwo()
+                               );
+
+        var expected = pricePerItem * 2 + pricePerItem;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+
+    [Fact]
+    public void when_quantity_is_five_three_items_get_discounted_and_one_item_gets_no_discount()
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                    ProductPrice.FromDecimal(pricePerItem),
+                                     ProductQuantity.FromInt(5),
+                                     new ThreeForThePriceOfTwo()
+                               );
+
+        var expected = 2 * pricePerItem + pricePerItem + pricePerItem;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+}
---
+++ Solution/Domain/ShoppingCart.cs
@@ -0,0 +1,138 @@
+﻿namespace Solution.Domain;
+
+public class ShoppingCart
+{
+    public IList<Product> Products { get; private set; }
+
+    private ShoppingCart()
+    {
+        Products = new List<Product>();
+    }
+    public void StoreProduct(Product product)
+    {
+        Products.Add(product);
+    }
+
+    public int GetNumberOfProducts() => Products.Count;
+
+    public static ShoppingCart CreateEmptyCart() => new ShoppingCart();
+
+    public decimal GetSumOfItems()
+    {
+        return Products.Sum(p => p.GetPriceWithDiscount());
+    }
+}
+
+
+public class Product
+{
+    public ProductName Name { get; private set; }
+    public ProductPrice Price { get; private set; }
+
+    // TODO :: implement as a value ofje
+    public ProductQuantity Quantity { get; private set; }
+
+    public IDiscountStrategy DiscountStrategy { get; private set; }
+
+    public Product(ProductName name, ProductPrice price, ProductQuantity? quantity, IDiscountStrategy discountStrategy)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity ?? new ProductQuantity(1);
+        DiscountStrategy = discountStrategy;
+    }
+
+    public bool HasADiscount => DiscountStrategy is not null;
+
+    /// <summary>
+    /// Returns the discounted price is the product has a discount
+    /// Otherwise returns the product price * product quantity
+    /// </summary>
+    /// <returns></returns>
+    public decimal GetPriceWithDiscount()
+    {
+        return HasADiscount
+            ? DiscountStrategy.GetDiscountedPrice(this)
+            : GetTotalPrice();
+    }
+
+    private decimal GetTotalPrice()
+    {
+        return Price.Value * Quantity.Value;
+    }
+
+}
+
+public class ProductName : ValueObject
+{
+    public string Value { get; private set; }
+
+    public ProductName(string name)
+    {
+        Value = name;
+    }
+
+    public static ProductName FromString(string productName)
+    {
+        return new ProductName(productName);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+
+public class ProductPrice : ValueObject
+{
+    public decimal Value { get; private set; }
+
+    private ProductPrice(decimal amount)
+    {
+        if (amount < 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Product price have to be positive");
+
+        Value = amount;
+    }
+
+    public static ProductPrice FromDecimal(decimal amount) => new ProductPrice(amount);
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+
+public class ProductQuantity : ValueObject
+{
+    public int Value { get; private set; }
+
+    public ProductQuantity(int quantity)
+    {
+        if (quantity < 0)
+            throw new ArgumentOutOfRangeException("Cannot have a negetive quantity");
+
+        Value = quantity;
+    }
+    public static ProductQuantity FromInt(int quantity) => new ProductQuantity(quantity);
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+
+public interface IDiscountStrategy
+{
+    decimal GetDiscountedPrice(Product product);
+}
+
+public class ThreeForThePriceOfTwo : IDiscountStrategy
+{
+    public decimal GetDiscountedPrice(Product product)
+    {
+        var maxThreeItems = product.Quantity.Value / 3;
+        var remainingItems = product.Quantity.Value - maxThreeItems * 3;
+
+        return remainingItems * product.Price.Value + maxThreeItems * 2 * product.Price.Value;
+    }
+}
