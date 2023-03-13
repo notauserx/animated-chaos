1dd24eccbfd8c9bc20f106c7d9a0026da756d357
notauserx <notauserx@users.noreply.github.com>
2023-03-13 09:27:19 +0600

    move test classes to files

---
+++ Solution.Tests/ShoppingCartDiscountTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Solution.Tests;
+
+public class ShoppingCartDiscountTests
+{
+    [Fact]
+    public void test_case_one()
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        cart.StoreProduct(new Product(new ProductName("T-Shirt"),
+                                      ProductPrice.FromDecimal(10),
+                                      ProductQuantity.FromInt(2),
+                                      null));
+
+        cart.StoreProduct(new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(2),
+                                      null));
+
+        var discountStrategy = new TwoTShirtAndTwoJeansDiscountStrategy();
+
+        Assert.True(discountStrategy.IsApplicable(cart));
+
+        var discountedPrice = discountStrategy.GetDiscountedPrice(cart);
+
+        Assert.Equal(30, discountedPrice);
+
+
+    }
+}
---
+++ Solution.Tests/ThreeForThePriceOfTwoDiscountTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Solution.Tests;
+
+public class ThreeForThePriceOfTwoDiscountTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void when_less_than_three_items_discount_does_not_apply(int quantity)
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(pricePerItem),
+                                      ProductQuantity.FromInt(quantity),
+                                      new ThreeForThePriceOfTwo()
+                                );
+
+        var expected = pricePerItem * quantity;
+
+        Assert.Equal(expected, discountedProduct.GetPriceWithDiscount());
+    }
+
+    [Theory]
+    [InlineData(6)]
+    [InlineData(30)]
+    [InlineData(18)]
+    [InlineData(3)]
+    public void when_quantity_is_a_multiple_of_three_discount_applies_to_each_multiple(int quantity)
+    {
+        var pricePerItem = 20;
+        var multipleOfThree = quantity / 3;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(pricePerItem),
+                                      ProductQuantity.FromInt(quantity),
+                                      new ThreeForThePriceOfTwo()
+                                );
+
+        var expected = pricePerItem * 2 * multipleOfThree;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+
+    [Fact]
+    public void when_quantity_is_four_three_items_get_discounted_and_one_item_gets_no_discount()
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                    ProductPrice.FromDecimal(pricePerItem),
+                                     ProductQuantity.FromInt(4),
+                                     new ThreeForThePriceOfTwo()
+                               );
+
+        var expected = pricePerItem * 2 + pricePerItem;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+
+    [Fact]
+    public void when_quantity_is_five_three_items_get_discounted_and_one_item_gets_no_discount()
+    {
+        var pricePerItem = 20;
+        var discountedProduct = new Product(new ProductName("Jeans"),
+                                    ProductPrice.FromDecimal(pricePerItem),
+                                     ProductQuantity.FromInt(5),
+                                     new ThreeForThePriceOfTwo()
+                               );
+
+        var expected = 2 * pricePerItem + pricePerItem + pricePerItem;
+        var discountedPrice = discountedProduct.GetPriceWithDiscount();
+
+        Assert.Equal(expected, discountedPrice);
+    }
+}
