c3492bfada83729d37b68dd4518d573273221cc6
notauserx <notauserx@users.noreply.github.com>
2023-03-13 09:53:39 +0600

    update tests

--- Solution.Tests/ShoppingCartDiscountTests.cs
+++ Solution.Tests/ShoppingCartDiscountTests.cs
@@ -3,7 +3,34 @@
 public class ShoppingCartDiscountTests
 {
     [Fact]
-    public void test_case_one()
+    public void when_less_than_two_tshirt_and_two_jeans_in_cart_discounted_price_is_not_set()
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        cart.StoreProduct(new Product(new ProductName("T-Shirt"),
+                                      ProductPrice.FromDecimal(10),
+                                      ProductQuantity.FromInt(1),
+                                      null));
+
+        cart.StoreProduct(new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(2),
+                                      null));
+
+        var discountStrategy = new TwoTShirtAndTwoJeansDiscountStrategy();
+
+        Assert.False(discountStrategy.IsApplicable(cart));
+
+        var discountedPrice = discountStrategy.GetDiscountedPrice(cart);
+
+        // not setting discounted price for t - shirt 5HKD and jeans 10hkd
+        var expectedPrice = 1 * 10 + 2 * 20;
+
+        Assert.Equal(expectedPrice, discountedPrice);
+    }
+
+    [Fact]
+    public void when_two_tshirt_and_two_jeans_in_cart_discounted_price_is_set()
     {
         var cart = ShoppingCart.CreateEmptyCart();
 
@@ -23,6 +50,68 @@ public class ShoppingCartDiscountTests
 
         var discountedPrice = discountStrategy.GetDiscountedPrice(cart);
 
-        Assert.Equal(30, discountedPrice);
+        // setting discounted price for t - shirt 5HKD and jeans 10hkd
+        var expectedPrice = 2 * 5 + 2 * 10;
+
+        Assert.Equal(expectedPrice, discountedPrice);
+    }
+
+    [Fact]
+    public void only_two_set_items_in_cart__gets_discounted_price()
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        cart.StoreProduct(new Product(new ProductName("T-Shirt"),
+                                      ProductPrice.FromDecimal(10),
+                                      ProductQuantity.FromInt(3),
+                                      null));
+
+        cart.StoreProduct(new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(3),
+                                      null));
+
+        var discountStrategy = new TwoTShirtAndTwoJeansDiscountStrategy();
+
+        Assert.True(discountStrategy.IsApplicable(cart));
+
+        var discountedPrice = discountStrategy.GetDiscountedPrice(cart);
+
+        // setting discounted price for t - shirt 5HKD and jeans 10hkd for 2 t-shirts and 2-jeans
+        var expectedPrice = 2 * 5 + 2 * 10 + 1 * 10 + 1 * 20;
+
+        Assert.Equal(expectedPrice, discountedPrice);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(10)]
+    public void all_two_set_items_in_cart__gets_discounted_price(int setCount)
+    {
+        var cart = ShoppingCart.CreateEmptyCart();
+
+        cart.StoreProduct(new Product(new ProductName("T-Shirt"),
+                                      ProductPrice.FromDecimal(10),
+                                      ProductQuantity.FromInt(2 * setCount),
+                                      null));
+
+        cart.StoreProduct(new Product(new ProductName("Jeans"),
+                                     ProductPrice.FromDecimal(20),
+                                      ProductQuantity.FromInt(2 * setCount),
+                                      null));
+
+        var discountStrategy = new TwoTShirtAndTwoJeansDiscountStrategy();
+
+        Assert.True(discountStrategy.IsApplicable(cart));
+
+        var discountedPrice = discountStrategy.GetDiscountedPrice(cart);
+
+        // setting discounted price for t - shirt 5HKD and jeans 10hkd for 2 t-shirts and 2-jeans
+        var expectedPrice = (2 * 5 * setCount) + (2 * 10 * setCount);
+
+        Assert.Equal(expectedPrice, discountedPrice);
     }
 }
