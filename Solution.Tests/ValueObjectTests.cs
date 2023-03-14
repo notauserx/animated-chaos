c05254695483f6d4ae4ec48d06b3cca3dd653ee4
notauserx <notauserx@users.noreply.github.com>
2023-03-14 08:07:34 +0600

    refactor: move dummy object to test project

---
+++ Solution.Tests/ValueObjectTests.cs
@@ -0,0 +1,63 @@
+﻿using Solution.Framework;
+
+namespace Solution.Tests;
+
+/// <summary>
+/// A value object for demo purposes
+/// </summary>
+public class DummyObject : ValueObject
+{
+    public string Value { get; private set; }
+
+    public DummyObject(string value)
+    {
+        Value = value;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+public class ValueObjectTests
+{
+    private static DummyObject One = new ("abc");
+    private static DummyObject SameAsOne = new ("abc");
+    private static DummyObject Another = new ("ab");
+
+    [Fact]
+    public void value_object_with_same_values_should_be_equal()
+    {
+        Assert.Equal(One, SameAsOne);
+    }
+
+    [Fact]
+    public void value_object_with_different_values_should_not_be_equal()
+    {
+        Assert.NotEqual(One, Another);
+    }
+
+    [Fact]
+    public void comparing_value_object_with_same_values_with_equality_operator_should_return_true()
+    {
+        Assert.True(One == SameAsOne);
+    }
+
+    [Fact]
+    public void comparing_value_object_with_same_values_with_non_equality_operator_should_return_false()
+    {
+        Assert.False(One != SameAsOne);
+    }
+
+    [Fact]
+    public void comparing_value_object_with_different_values_with_non_equality_operator_should_return_true()
+    {
+        Assert.True(One != Another);
+    }
+
+    [Fact]
+    public void comparing_value_object_with_different_values_with_equality_operator_should_return_false()
+    {
+        Assert.False(One == Another);
+    }
+}
